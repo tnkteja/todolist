8a0758152976db227c1daca17e003bddc1f1bc87
neela krishna teja tadikonda <neelakrishnatejatadikonda@neelas-MacBook-Air.local>
2023-06-10 14:02:45 +0530

    removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Todo from "./components/Todo"
-import TodoList from './components/TodoList';
 import {Routes, Route} from "react-router-dom"
 import Home from './pages/Home';
 import Login from './pages/Login';
